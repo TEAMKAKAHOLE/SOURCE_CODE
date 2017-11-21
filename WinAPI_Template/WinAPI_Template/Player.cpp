903c1db6e8cf3c0a3ee62a9e95e01d80026f725c
komastar <komastar.dev@gmail.com>
2017-11-21 11:52:27 +0900

    플레이어 애니메이션 구현. IDLE/RUN/ATK

--- WinAPI_Template/WinAPI_Template/Player.cpp
+++ WinAPI_Template/WinAPI_Template/Player.cpp
@@ -4,8 +4,9 @@
 
 Player::Player()
 {
+    m_playerStatus = PLAYER_IDLE;
+    m_weaponType = WEAPON_SWORD;
 }
-
 
 Player::~Player()
 {
@@ -18,10 +19,31 @@ void Player::Start()
 
 void Player::Update()
 {
-    isAnimate = false;
     PlayerController();
-    if (isAnimate == false)
+
+    switch (m_playerStatus)
+    {
+    case PLAYER_IDLE:
+    {
         SetFrameX(0);
+        isAnimate = false;
+        break;
+    }
+    case PLAYER_RUN:
+    {
+        frameDelay = 6;
+        isAnimate = true;
+        break;
+    }
+    case PLAYER_ATTACK:
+    {
+        frameDelay = 3;
+        maxFrameX = PLAYER_ATK_MAX_FRAME;
+        frameY = m_currFrameY + 1;
+        isAnimate = true;
+        break;
+    }
+    }
 
     SpritesObject::Update();
 }
@@ -36,28 +58,41 @@ void Player::PlayerController()
     UnitSpeed speed = { 0.0f, 0.0f };
     if (g_pKeyManager->isStayKeyDown(VK_LEFT))
     {
+        m_currFrameY = 4;
         speed.x -= 5.0f;
-        SetFrameY(4);
-        isAnimate = true;
+        m_playerStatus = PLAYER_RUN;
     }
     else if (g_pKeyManager->isStayKeyDown(VK_RIGHT))
     {
+        m_currFrameY = 2;
         speed.x += 5.0f;
-        SetFrameY(2);
-        isAnimate = true;
+        m_playerStatus = PLAYER_RUN;
     }
     else if (g_pKeyManager->isStayKeyDown(VK_UP))
     {
+        m_currFrameY = 6;
         speed.y -= 5.0f;
-        SetFrameY(6);
-        isAnimate = true;
+        m_playerStatus = PLAYER_RUN;
     }
     else if (g_pKeyManager->isStayKeyDown(VK_DOWN))
     {
+        m_currFrameY = 0;
         speed.y += 5.0f;
-        SetFrameY(0);
+        m_playerStatus = PLAYER_RUN;
+    }
+    else if (g_pKeyManager->isStayKeyDown(VK_SPACE))
+    {
+        if (m_playerStatus == PLAYER_RUN)
+            frameX = 0;
+
+        m_playerStatus = PLAYER_ATTACK;
         isAnimate = true;
     }
+    else
+    {
+        m_playerStatus = PLAYER_IDLE;
+    }
 
+    frameY = m_currFrameY;
     m_dSpeed = speed;
 }
--- WinAPI_Template/WinAPI_Template/Player.h
+++ WinAPI_Template/WinAPI_Template/Player.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "SpritesObject.h"
+
+#define PLAYER_RUN_MAX_FRAME 6
+#define PLAYER_ATK_MAX_FRAME 5
+
 class Player : public SpritesObject
 {
+private:
+    E_PLAYER_STATUS m_playerStatus;
+    E_WEAPON_TYPE   m_weaponType;
+    int             m_currFrameY;
+
 public:
     Player();
     ~Player();
