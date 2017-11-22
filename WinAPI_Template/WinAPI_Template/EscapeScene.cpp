cff1493ed13b2963b7225a31dba051027186a7e7
kimdaehwan <mycol75@naver.com>
2017-11-22 14:27:06 +0900

    start&end Point add

--- WinAPI_Template/WinAPI_Template/EscapeScene.cpp
+++ WinAPI_Template/WinAPI_Template/EscapeScene.cpp
@@ -19,6 +19,7 @@ void EscapeScene::Start()
 	m_imgWorldBuffer     = g_pImgManager->AddImage("eacape-map-buffer", 512, 512);
 	m_imgWorldMap        = g_pImgManager->FindImage("escape-map");
 	m_imgTerrainBuffer   = g_pImgManager->FindImage("escape-terrain");
+
 	m_player.SetBodyImg(g_pImgManager->FindImage("player"));
 	m_player.SetupForSprites(7,8);
 	m_player.StartAnimation();
@@ -31,13 +32,7 @@ void EscapeScene::Start()
     m_player.SetLockArea({ 0, 0, 512, 512 });
     m_player.LockInWnd();
 
-	//Player스타트 위치 세팅 512사이즈위치 *4 
-
-	//for (int i = 0; i < 4; ++i)
-	//{
-	//	TelePort m_teleport;
-
-	//}  일단 하드코딩
+	
 
 	m_teleportA.SetBodyPos({ (int)385, (int)300 });
 	m_teleportA.SetBodySize({ (int)20, (int)20 });
@@ -53,6 +48,23 @@ void EscapeScene::Start()
 	
 	m_teleportD.SetBodyPos({ (int)95, (int)285 });
 	m_teleportD.SetBodySize({ (int)20, (int)20 });
+
+	//next Scene 이동위한 Endpoint
+	m_EndPoint.SetBodyPos({ 80 ,170 });
+	m_EndPoint.SetBodySize({ 20,20 });
+	m_StartPoint.SetBodyPos({ 350 ,390 });
+	m_StartPoint.SetBodySize({ 20,20 });
+
+
+	//Item 초기위치,bool 설정
+	m_KeyItem1.SetBodyImg(g_pImgManager->FindImage("keyItem"));
+	m_KeyItem1.SetupForSprites(1,1);
+	m_KeyItem1.StartAnimation();
+	m_KeyItem1.SetBodySize({ 46,46 });
+	m_KeyItem1.SetBodyPos({ 420,260 });
+	m_bKeyItem1 = false;
+	
+
 }
 
 void EscapeScene::Update()
@@ -62,6 +74,9 @@ void EscapeScene::Update()
 	m_teleportB.Update();
 	m_teleportC.Update();
 	m_teleportD.Update();
+	m_KeyItem1.Update();
+	m_EndPoint.Update();
+	m_StartPoint.Update();
 
 	int distance = 10;
 
@@ -84,6 +99,18 @@ void EscapeScene::Update()
 	{
 		m_player.SetBodyPos({ (int)385, (int)300 + 50 });
 	}
+	else if (IntersectRect(&rt, &m_player.GetBodyRect(), &m_EndPoint.GetBodyRect()))
+	{  //씬 이동을 위한 충돌 판단 부분
+		if(m_bKeyItem1)
+		{
+		  SceneChange();
+		}
+	}
+	else if (IntersectRect(&rt, &m_player.GetBodyRect(), &m_KeyItem1.GetBodyRect()))
+	{
+		m_bKeyItem1 = true;
+		m_KeyItem1.SetInvisible();
+	}
 }
 
 void EscapeScene::Render()
@@ -96,10 +123,19 @@ void EscapeScene::Render()
 	m_teleportB.Render(m_imgWorldBuffer->GetMemDC());
 	m_teleportC.Render(m_imgWorldBuffer->GetMemDC());
 	m_teleportD.Render(m_imgWorldBuffer->GetMemDC());
+	m_KeyItem1.Render(m_imgWorldBuffer->GetMemDC());
+	m_EndPoint.Render(m_imgWorldBuffer->GetMemDC());
+	m_StartPoint.Render(m_imgWorldBuffer->GetMemDC());
 
 	m_imgWorldBuffer->ViewportRender(g_hDC, g_rtViewPort);
 }
 
 void EscapeScene::Release()
 {
+}
+
+void EscapeScene::SceneChange()
+{
+	g_pScnManager->SetNextScene("puzzle");
+	g_pScnManager->ChangeScene("loading");
 }
--- WinAPI_Template/WinAPI_Template/EscapeScene.h
+++ WinAPI_Template/WinAPI_Template/EscapeScene.h
@@ -11,8 +11,17 @@ private:
 	ImageObject* m_imgWorldBuffer;
 	ImageObject* m_imgWorldMap;
 	ImageObject* m_imgTerrainBuffer;
-
+	GameObject  m_EndPoint;   // 80,170
+	GameObject  m_StartPoint; // 350,390 
 	Player   m_player;
+
+
+	//마지막 탈출시 필요한 열쇠아이템 RECT이미지
+	SpritesObject m_KeyItem1;
+	//GameObject m_KeyItem2;
+	bool m_bKeyItem1;
+	//bool m_bKeyItme2;
+
 	TelePort m_teleportA;
 	TelePort m_teleportB;
 	TelePort m_teleportC;
@@ -27,5 +36,6 @@ public:
 	virtual  void  Update()   override;
 	virtual  void  Render()   override;
 	virtual  void  Release()  override;
+	void  SceneChange();
 };
 
