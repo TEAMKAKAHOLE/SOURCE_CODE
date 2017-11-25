0fe28bac032444356d7909047daaa0af1dacf2d0
komastar <komastar.dev@gmail.com>
2017-11-25 19:50:30 +0900

    ending credit make viewport

--- WinAPI_Template/WinAPI_Template/EndingCreditScene.cpp
+++ WinAPI_Template/WinAPI_Template/EndingCreditScene.cpp
@@ -7,17 +7,16 @@ EndingCreditScene::EndingCreditScene()
     m_szTagName = "ending-credit";
     g_pImgManager->AddImageList(m_szTagName);
     while (g_pImgManager->AddImageByJson(m_szTagName));
-
+    m_imgEndingCreditBuffer = g_pImgManager->AddImage("ending-credit-buffer", CREDIT_WIDTH, CREDIT_HEIGHT);
     m_sprEndingCredit = NULL;
+    m_dViewPortPos = GLOBAL_CENTER_POS;
 }
 
 
 EndingCreditScene::~EndingCreditScene()
 {
     if (m_sprEndingCredit != NULL)
-    {
         SAFE_DELETE(m_sprEndingCredit);
-    }
 }
 
 void EndingCreditScene::Start()
@@ -27,28 +26,37 @@ void EndingCreditScene::Start()
     m_sprEndingCredit->SetBodyPos({ (double)W_WIDTH * 0.5f
         , (double)W_HEIGHT * 0.5f + (double)m_sprEndingCredit->GetBodyImg()->GetHeight() * 0.5f });
     m_sprEndingCredit->SetBodySpeed({ 0.0f, -1.0f });
-    m_sprEndingCredit->SetBodySize({ 768, 8200 });
+    m_sprEndingCredit->SetBodySize({ CREDIT_WIDTH, CREDIT_HEIGHT });
     m_sprEndingCredit->SetupForSprites(1, 1);
+
+    m_sprEndingCredit->GetBodyImg()->FastRender(m_imgEndingCreditBuffer->GetMemDC(), 0, 0);
+
+    m_nVpMargin.left = -(W_WIDTH - VIEWPORT_WIDTH) / 2;
+    m_nVpMargin.right = m_nVpMargin.left;
+    m_nVpMargin.top = -(W_HEIGHT - VIEWPORT_HEIGHT) / 2;
+    m_nVpMargin.bottom = m_nVpMargin.top;
 }
 
 void EndingCreditScene::Update()
 {
-    m_sprEndingCredit->Update();
-    if (m_sprEndingCredit->GetBodyRect().bottom <= 0)
+    m_dViewPortPos.y += CREDIT_SCROLL_SPEED;
+    if (m_dViewPortPos.y >= CREDIT_HEIGHT)
     {
         g_pScnManager->ChangeScene("cookie");
     }
+
+    g_rtViewPort = g_pDrawHelper->MakeViewPort(m_dViewPortPos, m_nVpMargin, m_imgEndingCreditBuffer);
 }
 
 void EndingCreditScene::Render()
 {
     PatBlt(g_hDC, 0, 0, W_WIDTH, W_HEIGHT, BLACKNESS);
-    m_sprEndingCredit->GetBodyImg()->FastRender(g_hDC
-        , m_sprEndingCredit->GetBodyRect().left, m_sprEndingCredit->GetBodyRect().top);
-
+    m_imgEndingCreditBuffer->ViewportRender(g_hDC, g_rtViewPort);
 #ifdef _DEBUG
-    string szPosY = to_string(m_sprEndingCredit->GetBodyPos().y);
-    TextOut(g_hDC, 0, 100, szPosY.c_str(), (int)strlen(szPosY.c_str()));
+    string szPosY = to_string(m_dViewPortPos.y);
+    TextOut(g_hDC, 0, 100, szPosY.c_str(), (int)strlen(szPosY.c_str())); 
+    szPosY = to_string(g_rtViewPort.top);
+    TextOut(g_hDC, 0, 115, szPosY.c_str(), (int)strlen(szPosY.c_str()));
 #endif // _DEBUG
 }
 
--- WinAPI_Template/WinAPI_Template/EndingCreditScene.h
+++ WinAPI_Template/WinAPI_Template/EndingCreditScene.h
@@ -2,10 +2,17 @@
 #include "GameNode.h"
 #include "SpritesObject.h"
 
+#define CREDIT_SCROLL_SPEED     1.0f
+#define CREDIT_WIDTH            W_WIDTH
+#define CREDIT_HEIGHT           9000
+
 class EndingCreditScene : public GameNode
 {
 private:
+    ImageObject*    m_imgEndingCreditBuffer;
     SpritesObject*  m_sprEndingCredit;
+    UnitPos         m_dViewPortPos;
+    RectMargin      m_nVpMargin;
 
 public:
     EndingCreditScene();
